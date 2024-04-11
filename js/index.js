3700590e64d8518375d803d1f0ea5cd2eb371986
EricB-59 <ebaenac59@gmail.com>
2024-04-11 09:49:40 +0200

    Eric

--- js/index.js
+++ js/index.js
@@ -1,16 +1,57 @@
-$("#button-AjustarCuenta").click(function(){
-    let estadoActual = $("#ajustarCuenta_ALL").css("display");
+// DESPLEGABLE DE AJUSTAR CUENTA
+$("#IMG-FLECHA-MENU").click(function(){
+    let estadoActual = $(".container-ajustar ").css("display");
     if (estadoActual === "none") {
-        $("#ajustarCuenta_ALL").css("display", "inline-block");
+        $(".container-ajustar").slideDown();
+        $(".container-ajustar").css("display", "inline-block");
     }else{
-        $("#ajustarCuenta_ALL").css("display", "none");
+        $(".container-ajustar").slideUp();
+        $(".container-ajustar").css("display", "none");
     }
 });
 
-$("#DIV-sliderNovedades").slick({
-    dots: true,
-    infinity: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
+// VALIDATE AJUSTAR CUENTA
+$("#FORM-AJUSTARCUENTA").validate({
+    rules: {
+        nombreUsuario:{
+            required: true,
+            minlength: 5,
+            maxlength: 16
+        },
+        correoElectronico:{
+            required: true,
+            email: true
+        },
+        password:{
+            required: true,
+            minlength: 5,
+            maxlength: 20
+        },
+        repeatPassword:{
+            required: true,
+            minlength: 5,
+            maxlength: 200
+        }
+    },
+    messages: {
+        nombreUsuario: {
+            required: "<div class=ERROR-VALIDATE>INTRODUCE UN NOMBRE</div>",
+            minlength: "<div class=ERROR-VALIDATE>El nombre debe tener minimo 5 caracter</div>",
+            maxlength: "<div class=ERROR-VALIDATE>El nombre debe tener maximo 16 caracter</div>"
+        },
+        correoElectronico:{
+            required: "<div class=ERROR-VALIDATE>INTRODUCE UN EMAIL</div>",
+            email: "<div class=ERROR-VALIDATE>INTRODUCE UNA DIRRECCIÓN VALIDA DE CORREO ELECTRONICO</div>"
+        },
+        password:{
+            required: "<div class=ERROR-VALIDATE>INTRODUCE UNA CONTRASEÑA</div>",
+            minlength: "<div class=ERROR-VALIDATE>La contraseña debe tener mínimo 5 carácter</div>",
+            maxlength: "<div class=ERROR-VALIDATE>La contraseña debe tener máximo 20 carácter</div>"
+        },
+        repeatPassword:{
+            required: "<div class=ERROR-VALIDATE>INTRODUCE UNA CONTRASEÑA</div>",
+            minlength: "<div class=ERROR-VALIDATE>La contraseña debe tener mínimo 5 carácter</div>",
+            maxlength: "<div class=ERROR-VALIDATE>La contraseña debe tener máximo 20 carácter</div>"
+        }
+    }
 });
