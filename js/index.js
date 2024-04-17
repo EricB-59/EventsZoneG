60faee0ae94a50c86c0725b2d94fb6ce3e5ec787
EricB-59 <ebaenac59@gmail.com>
2024-04-17 11:12:09 +0200

    Eric

--- js/index.js
+++ js/index.js
@@ -222,3 +222,25 @@ $("#FORM-LOGINCUENTA").validate({
         },
     }
 });
+
+// COOKIES
+var cookiesAceptadas = localStorage.getItem('cookiesAceptadas');
+$('.login-registro').hide();
+$('.mensaje-rechazo-cookies').hide();
+
+$('.aceptar-cookies').click(function(){
+    localStorage.setItem('cookiesAceptadas', true);
+    $('.mensaje-cookies').hide();
+    $('.mensaje-rechazo-cookies').hide();
+    $('.login-registro').show();
+})
+$('.rechazar-cookies').click(function(){
+    localStorage.setItem('cookiesAceptadas',false);
+    $('.mensaje-cookies').hide();
+    $('.mensaje-rechazo-cookies').show();
+    $('.login-registro').hide();
+})
+$('.mensaje-rechazo-cookies').click(function(){
+    $('.mensaje-cookies').show();
+    $(this).hide();
+})
